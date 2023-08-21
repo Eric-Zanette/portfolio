216706b7d8166671cc36224374afe201a8d7a4a3
Eric-Zanette <ericzanette@gmail.com>
2023-08-21 14:20:28 -0400

    about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,75 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="container">
+      <div className="aboutPage">
+        <div className="pageHeader">About Me</div>{" "}
+        <img className="aboutPortrait" src="./img/eric.jpg" alt="" />
+        <div className="aboutText">
+          <p>
+            Dedicated to the ever-evolving realm of web development, I am a full
+            stack developer with expertise in JavaScript, React, and Python. My
+            approach combines deep technical knowledge with a keen understanding
+            of user-centric design. The challenge and satisfaction of turning
+            complex requirements into intuitive digital solutions fuels my
+            passion every day.
+          </p>
+        </div>
+        <h2>Technologies</h2>
+        <div className="aboutTechs">
+          <div className="tech">
+            <img src="./img/techs/aws.png" alt="" />
+            <p>AWS</p>
+          </div>
+          <div className="tech">
+            <img src="./img/techs/css.png" alt="" />
+            <p>CSS</p>
+          </div>
+          <div className="tech">
+            <img src="./img/techs/express.png" alt="" />
+            <p>Express</p>
+          </div>
+          <div className="tech">
+            <img src="./img/techs/flask.png" alt="" />
+            <p>Flask</p>
+          </div>
+          <div className="tech">
+            <img src="./img/techs/html.png" alt="" />
+            <p>HTML</p>
+          </div>
+          <div className="tech">
+            <img src="./img/techs/js.png" alt="" />
+            <p>Javascript</p>
+          </div>
+          <div className="tech">
+            <img src="./img/techs/node.png" alt="" />
+            <p>Node JS</p>
+          </div>
+          <div className="tech">
+            <img src="./img/techs/postgres.png" alt="" />
+            <p>Postgres</p>
+          </div>
+          <div className="tech">
+            <img src="./img/techs/python.png" alt="" />
+            <p>Python</p>
+          </div>
+          <div className="tech">
+            <img src="./img/techs/react.png" alt="" />
+            <p>React</p>
+          </div>
+          <div className="tech">
+            <img src="./img/techs/linux.png" alt="" />
+            <p>Linux</p>
+          </div>
+          <div className="tech">
+            <img src="./img/techs/mongo.png" alt="" />
+            <p>MongoDB</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -4,7 +4,90 @@ const Projects = () => {
   return (
     <div className="container">
       <div className="projectsPage">
-        <h1>My Projects</h1>
+        <h1 className="pageHeader">My Projects</h1>
+        <div className="projectLine ">
+          <div className="projectContainer chesster">
+            <h2 className="projectHeader">Chesster</h2>
+            <div className="projectContent">
+              <img src="./img/chesster.png" className="projectImage" alt="" />
+              <p>
+                A full stack chess application. User's can login, create and
+                join games, and keep track of their previous matches. React on
+                the frontend using flask on the backend with websocket, the
+                chess logic was built from scratch.
+              </p>
+              <div className="techsUsed"></div>
+            </div>
+          </div>
+          <div className="projectLinks chesster">
+            <a
+              href="https://github.com/Eric-Zanette/ChessSite"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3>GitHub</h3>
+            </a>
+            <a
+              href="http://35.183.69.138"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3>Live Site</h3>
+            </a>
+          </div>
+        </div>
+        <div className="projectLine">
+          <div className="projectContainer commster">
+            <h2 className="projectHeader">Commster</h2>
+            <div className="projectContent">
+              <p>
+                Full stack market place website. User's can login, post sales,
+                view recent sales, add items to cart and checkout. This project
+                uses React on the front end and Express JS with a Postgres
+                database on the backend.
+              </p>
+              <img src="./img/commster.png" className="projectImage" alt="" />
+            </div>
+          </div>
+          <div className="projectLinks left commster">
+            <a
+              href="https://github.com/Eric-Zanette/Commster"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3>GitHub</h3>
+            </a>
+            <a href="/" target="_blank" rel="noopener noreferrer">
+              <h3>Live Site</h3>
+            </a>
+          </div>
+        </div>
+        <div className="projectLine">
+          <div className="projectContainer tasker">
+            <h2 className="projectHeader">Tasker</h2>
+            <div className="projectContent">
+              <img src="./img/tasker.png" className="projectImage" alt="" />
+              <p>
+                Full stack task setting applicaton. User's can login, set tasks,
+                edit tasks, mark tasks complete and delete tasks. The project
+                uses react on the front end and Express JS on the backend with a
+                firebase database.
+              </p>
+            </div>
+          </div>
+          <div className="projectLinks tasker">
+            <a
+              href="https://github.com/Eric-Zanette/Tasker"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3>GitHub</h3>
+            </a>
+            <a href="/" target="_blank" rel="noopener noreferrer">
+              <h3>Live Site</h3>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
